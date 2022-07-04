35030b592450c75e69a1d2880a895783d815ace9
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2022-07-04 14:42:43 +0200

    call icon

--- layouts/Call.jsx
+++ layouts/Call.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Link from "next/link"
 
 const Call = () => {
   return (
-    <div className="call-icon">
-      <i className="fab fa-phone"></i>
-    </div>
+    <Link href="tel:+201020384108">
+      <a className="call-icon shadow rounded-pill d-flex">
+        <i className="fas fa-phone-alt"></i>
+      </a>
+    </Link>
   )
 }
 
