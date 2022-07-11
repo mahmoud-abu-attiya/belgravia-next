f6e7297e00c6d62f99d44868aae02d077fdab035
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2022-07-11 18:37:04 +0200

    edit menu swiper 2

--- components/Swip.jsx
+++ components/Swip.jsx
@@ -4,31 +4,34 @@ import "swiper/css/effect-cards";
 import { EffectCards } from "swiper";
 import Image from "next/image";
 import Link from "next/link";
-import fork from "../public/images/fork.png"
-import knife from "../public/images/knife.png"
+import fork from "../public/images/fork.png";
+import knife from "../public/images/knife.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const myLoader = ({ src }) => {
+  return src;
+};
 const Swip = () => {
   let [swipImage, setImages] = useState([]);
   useEffect(() => {
-    axios.get('http://44.208.45.254/api/home/products/').then(res => setImages(res.data))
+    axios
+      .get("http://44.208.45.254/api/home/products/")
+      .then((res) => setImages(res.data));
   }, []);
 
   return (
     <section className="dishes bg-light py-5">
       <div className="container text-center">
         <h2>Discover our Dishes</h2>
-        <p>
-        Take a look at our menu to induce and view our divine dishes. 
-        </p>
+        <p>Take a look at our menu to induce and view our divine dishes.</p>
         <Swiper
           effect={"cards"}
           grabCursor={true}
           modules={[EffectCards]}
           className="mySwiper"
           data-aos="zoom-in-up"
-          style={{width: 280, height: 340}}
+          style={{ width: 280, height: 340 }}
         >
           <div className="tool fork">
             <Image src={fork} alt="fork" />
@@ -38,12 +41,21 @@ const Swip = () => {
           </div>
           {swipImage.map((el) => (
             <SwiperSlide key={el.id} className="shadow">
-              <Image src={el.image} layout="fill" alt="belgravia food" className="image_border" priority />
+              <Image
+                src={el.image}
+                layout="fill"
+                alt="belgravia food"
+                className="image_border"
+                loader={myLoader}
+                priority
+              />
             </SwiperSlide>
           ))}
         </Swiper>
         <Link href="/menu">
-          <a data-aos="fade-up" className="btn btn-outline btn-lg shadow mt-4">View our menu</a>
+          <a data-aos="fade-up" className="btn btn-outline btn-lg shadow mt-4">
+            View our menu
+          </a>
         </Link>
       </div>
     </section>
