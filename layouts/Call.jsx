4bfd6c8d2dbd7fbd4637b6bef8730318ec079543
Mahmoud Abu-Attiya <76923704+mahmoud-abu-attiya@users.noreply.github.com>
2022-07-23 00:58:44 +0200

    Update Call.jsx

--- layouts/Call.jsx
+++ layouts/Call.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Call = () => {
   return (
-    <Link href="tel:+201020384108">
+    <Link href="tel:+97444445660">
       <a className="call-icon shadow rounded-pill d-flex">
         <i className="fas fa-phone-alt"></i>
       </a>
